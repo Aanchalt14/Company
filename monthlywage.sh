dd7debe444a9eae4c63706fd58f91bbd42398dc6
Aanchalt14 <aanchalt71@gmail.com>
2020-11-21 23:32:49 +0530

    Added : monthly wage of employee

---
+++ monthlywage.sh
@@ -0,0 +1,17 @@
+NUM_WORKING_DAYS=20
+totalSalary=0
+for(( day=1 ; day <= $NUM_WORKING_DAYS; day++ ))
+do
+random=$(( RANDOM%3 ))
+case $random in
+$IS_PART_TIME) empHrs=4
+;;
+$IS_FULL_TIME) empHrs=8
+;;
+*) empHrs=0
+esac
+salary=$(($empHrs * $EMP_RATE_PER_HR))
+echo "Day: $day Salary: $salary"
+totalSalary=$(( $totalSalary + $salary ))
+done
+echo "Total Salary: $totalSalary"
